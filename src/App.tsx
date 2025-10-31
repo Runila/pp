e29263349e8bbc1cdd37fb23238be4f0bed5fe52
Runila <manyakingeorg@gmail.com>
2025-10-31 15:21:57 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BallGame from './components/BallGame';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BallGame />
+    </div>
+  );
+}
+
+export default App;
